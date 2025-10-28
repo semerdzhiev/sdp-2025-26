28bd715444619cf348843a89a0b9e795277e60f8
Atanas Semerdzhiev <semerdjiev@live.com>
2025-10-28 23:43:35 +0200

    lectures: small changes to fixed_size_array and dynamic_array

--- lectures/src/containers/include/containers/dynamic_array.h
+++ lectures/src/containers/include/containers/dynamic_array.h
@@ -2,158 +2,164 @@
 
 #include "fixed_size_array.h"
 
+#include <cassert>
+
 namespace dsa {
 
 template <typename T>
 class dynamic_array {
 
-	fixed_size_array<T> m_data;
-	size_t m_used = 0;
+    fixed_size_array<T> m_data;
+    size_t m_used = 0;
 
 public:
 
-	/// Thrown when an operation, that requires the array to have at least one element,
-	/// was performed on an empty array.
-	class EmptyArrayException : public std::logic_error {
-	public:
-		EmptyArrayException()
-			: std::logic_error("Operation was performed on an empty array")
-		{}
-	};
+    /// Thrown when an operation, that requires the array to have at least one element,
+    /// was performed on an empty array.
+    class EmptyArrayException : public std::logic_error {
+    public:
+        EmptyArrayException()
+            : std::logic_error("Operation was performed on an empty array")
+        {}
+    };
 
 public:
-	/// Constructs an empty array with zero capacity
-	dynamic_array() = default;
+    /// Constructs an empty array with zero capacity
+    dynamic_array() = default;
 
-	/// Constructs an array with size and capacity equal to initialSize
-	/// @exception std::bad_alloc Memory allocation failed
-	explicit dynamic_array(size_t initialCapacity)
-		: m_data(initialCapacity), m_used(initialCapacity)
-	{}
+    /// Constructs an array with size and capacity equal to initialSize
+    /// @exception std::bad_alloc Memory allocation failed
+    explicit dynamic_array(size_t initialCapacity)
+        : m_data(initialCapacity), m_used(initialCapacity)
+    {}
 
-	dynamic_array(const dynamic_array&) = default;
-	dynamic_array& operator=(const dynamic_array&) = default;
-	
-	dynamic_array(dynamic_array&& other)
-		: m_data(std::move(other.m_data))
-	{
-		m_used = other.m_used;
-		other.m_used = 0;
-	}
+    // Copy operations
+    dynamic_array(const dynamic_array&) = default;
+    dynamic_array& operator=(const dynamic_array&) = default;
 
-	dynamic_array& operator=(dynamic_array&& other)
-	{
-		if (this != &other) {
-			m_data = std::move(other.m_data);
-			m_used = other.m_used;
-			other.m_used = 0;
-		}
-		
-		return *this;
-	}
+    // Move constructor
+    dynamic_array(dynamic_array&& other)
+        : m_data(std::move(other.m_data)),
+          m_used(other.m_used)
+    {
+        other.m_used = 0;
+    }
 
-	/// Number of elements stored in the array
-	size_t size() const noexcept {
-		return m_used;
-	}
+    // Move assignment
+    dynamic_array& operator=(dynamic_array&& other)
+    {
+        assert(this != & other); // self-assignment in move assignment is UB
 
-	/// Size of the underlying buffer
-	size_t capacity() const noexcept {
-		return m_data.size();
-	}
+        m_data = std::move(other.m_data);
 
-	/// Retrieve the element at index 
-	/// @exception std::out_of_range If the index is out of the bounds of the array 
-	T& at(size_t index)
-	{
-		return m_data.at(index);
-	}
+        m_used = other.m_used;
+        other.m_used = 0;
 
-	/// Retrieve the element at index 
-	/// @exception std::out_of_range If the index is out of the bounds of the array 
-	const T& at(size_t index) const
-	{
-		return m_data.at(index);
-	}
+        return *this;
+    }
 
-	/// Retrieve the element at index 
-	T& operator[](size_t index)
-	{
-		return m_data[index];
-	}
-	
-	/// Retrieve the element at index 
-	const T& operator[](size_t index) const
-	{
-		return m_data[index];
-	}
+    /// Number of elements stored in the array
+    size_t size() const noexcept {
+        return m_used;
+    }
 
-	/// Retrieve the underlying buffer
-	T* data() noexcept
-	{
-		return m_data.data();
-	}
+    /// Size of the underlying buffer
+    size_t capacity() const noexcept {
+        return m_data.size();
+    }
 
-	/// Retrieve the underlying buffer
-	const T* data() const noexcept
-	{
-		return m_data.data();
-	}
+    /// Retrieve the element at index
+    /// @exception std::out_of_range If the index is out of the bounds of the array
+    T& at(size_t index)
+    {
+        return m_data.at(index);
+    }
 
-	/// Append value to the array
-	void push_back(const T& value)
-	{
-		reserve(m_used + 1);
-		m_data[m_used++] = value;
-	}
+    /// Retrieve the element at index
+    /// @exception std::out_of_range If the index is out of the bounds of the array
+    const T& at(size_t index) const
+    {
+        return m_data.at(index);
+    }
 
-	/// Remove the last element from the array
-	void pop_back()
-	{
-		if (m_used == 0)
-			throw EmptyArrayException();
+    /// Retrieve the element at index
+    T& operator[](size_t index)
+    {
+        return m_data[index];
+    }
 
-		--m_used;
-	}
+    /// Retrieve the element at index
+    const T& operator[](size_t index) const
+    {
+        return m_data[index];
+    }
 
-	/// Ensure the underlying buffer has at least a minimal capacity
-	void reserve(size_t desiredCapacity)
-	{
-		if (desiredCapacity <= capacity())
-			return;
+    /// Retrieve the underlying buffer
+    T* data() noexcept
+    {
+        return m_data.data();
+    }
 
-		size_t newCapacity = std::max(desiredCapacity, capacity() * 2);
-		
-		resize_to(newCapacity);
-	}
-	
-	/// Set the size of the array to a specific value.
-	void resize(size_t desiredSize)
-	{
-		reserve(desiredSize);
-		m_used = desiredSize;
-	}
+    /// Retrieve the underlying buffer
+    const T* data() const noexcept
+    {
+        return m_data.data();
+    }
 
-	/// If possible, reduce the memory used by the array
-	void shrink_to_fit()
-	{
-		resize_to(m_used);
-	}
+    /// Append value to the array
+    void push_back(const T& value)
+    {
+        reserve(m_used + 1);
+        m_data[m_used++] = value;
+    }
 
-	/// Quickly swaps the contents of this object with that of another
-	void swap(dynamic_array& other)
-	{
-		m_data.swap(other.m_data);
-		std::swap(m_used, other.m_used);
-	}
+    /// Remove the last element from the array
+    void pop_back()
+    {
+        if (m_used == 0)
+            throw EmptyArrayException();
+
+        --m_used;
+    }
+
+    /// Ensure the underlying buffer has at least a minimal capacity
+    void reserve(size_t desiredCapacity)
+    {
+        if (desiredCapacity <= capacity())
+            return;
+
+        size_t newCapacity = std::max(desiredCapacity, capacity() * 2);
+
+        resize_to(newCapacity);
+    }
+
+    /// Set the size of the array to a specific value.
+    void resize(size_t desiredSize)
+    {
+        reserve(desiredSize);
+        m_used = desiredSize;
+    }
+
+    /// If possible, reduce the memory used by the array
+    void shrink_to_fit()
+    {
+        resize_to(m_used);
+    }
+
+    /// Quickly swaps the contents of this object with that of another
+    void swap(dynamic_array& other)
+    {
+        m_data.swap(other.m_data);
+        std::swap(m_used, other.m_used);
+    }
 
 private:
-	void resize_to(size_t desired_size)
-	{
-		fixed_size_array<T> buffer(desired_size);
-		buffer.fill_from(m_data);
-		m_data = std::move(buffer);
-	}
+    void resize_to(size_t desired_size)
+    {
+        fixed_size_array<T> buffer(desired_size);
+        buffer.fill_from(m_data);
+        m_data = std::move(buffer);
+    }
 };
 
 } // namespace
--- lectures/src/containers/include/containers/fixed_size_array.h
+++ lectures/src/containers/include/containers/fixed_size_array.h
@@ -4,141 +4,153 @@ namespace dsa {
 
 template <typename T>
 class fixed_size_array {
-	T* m_data = nullptr;
-	size_t m_size = 0;
+    T* m_data = nullptr;
+    size_t m_size = 0;
 
 public:
 
-	/// Constructs an empty array
-	fixed_size_array() noexcept = default;
+    /// Constructs an empty array
+    fixed_size_array() noexcept = default;
 
-	/// Creates an array with a specified size
-	/// @exception std::bad_alloc if memory allocation fails
-	explicit fixed_size_array(size_t size)
-	{
-		if (size != 0) {
-			m_data = new T[size];
-			m_size = size;
-		}
-	}
+    /// Creates an array with a specified size
+    /// @exception std::bad_alloc if memory allocation fails
+    explicit fixed_size_array(size_t size)
+    {
+        if (size != 0) {
+            m_data = new T[size];
+            m_size = size;
+        }
+    }
 
-	///
-	/// Copies the values from another array into the current object
-	/// 
-	/// The function copies min(size(), other.size()) elements
-	/// 
-	void fill_from(const fixed_size_array& other)
-	{
-		size_t limit = std::min(size(), other.size());
-			
-		for (size_t i = 0; i < limit; ++i)
-			m_data[i] = other.m_data[i];
-	}
+    ///
+    /// Copies the values from another array into the current object
+    ///
+    /// The function copies min(size(), other.size()) elements
+    ///
+    void fill_from(const fixed_size_array& other)
+    {
+        size_t limit = std::min(size(), other.size());
 
-	/// Creates a copy of another array
-	fixed_size_array(const fixed_size_array& other)
-		: fixed_size_array(other.m_size)
-	{
-		fill_from(other);
-	}
+        for (size_t i = 0; i < limit; ++i)
+            m_data[i] = other.m_data[i];
+    }
 
-	/// Copies the contents of another array
-	fixed_size_array& operator=(const fixed_size_array& other)
-	{
-		if(this != &other) {
-			fixed_size_array copy(other);
-			swap(copy);
-		}
+    /// Copy constructor
+    fixed_size_array(const fixed_size_array& other)
+        : fixed_size_array(other.m_size) //delegate memory allocation to the proper constructor
+    {
+        fill_from(other);
+    }
 
-		return *this;
-	}
+    /// Copy assignment
+    fixed_size_array& operator=(const fixed_size_array& other)
+    {
+        if(this != &other) {
+            fixed_size_array copy(other);
+            swap(copy);
+        }
 
-	fixed_size_array(fixed_size_array&& other) noexcept
-	{
-		swap(other);
-	}
+        return *this;
+    }
 
-	fixed_size_array& operator=(fixed_size_array&& other) noexcept
-	{
-		fixed_size_array temp(std::move(other));
-		swap(temp);
-		return *this;
-	}
+    /// Move constructor
+    fixed_size_array(fixed_size_array&& other) noexcept
+        : m_data(other.m_data), m_size(other.m_size)
+    {
+        other.m_data = nullptr;
+        other.m_size = 0;
+    }
 
-	~fixed_size_array() noexcept
-	{
-		delete [] m_data;
-	}
+    /// Move assignment
+    fixed_size_array& operator=(fixed_size_array&& other)
+    {
+        assert(this != &other); // self-assignment in move operations is UB
 
-	size_t size() const noexcept
-	{
-		return m_size;
-	}
+        delete [] m_data;
 
-	bool empty() const noexcept
-	{
-		return m_size == 0;
-	}
+        m_data = other.m_data;
+        m_size = other.m_size;
 
-	T* data() noexcept
-	{
-		return m_data;
-	}
+        other.m_data = nullptr;
+        other.m_size = 0;
 
-	const T* data() const noexcept
-	{
-		return m_data;
-	}
+        return *this;
+    }
 
-	T& at(size_t index)
-	{
-		if (index >= m_size)
-			throw std::out_of_range("index is out of the bounds of the array");
+    ~fixed_size_array() noexcept
+    {
+        delete [] m_data;
+    }
 
-		return m_data[index];
-	}
+    size_t size() const noexcept
+    {
+        return m_size;
+    }
 
-	const T& at(size_t index) const
-	{
-		if (index >= m_size)
-			throw std::out_of_range("index is out of the bounds of the array");
+    bool empty() const noexcept
+    {
+        return m_size == 0;
+    }
 
-		return m_data[index];
-	}
+    T* data() noexcept
+    {
+        return m_data;
+    }
 
-	T& operator[](size_t index) noexcept
-	{
-		return m_data[index];
-	}
+    const T* data() const noexcept
+    {
+        return m_data;
+    }
 
-	const T& operator[](size_t index) const noexcept
-	{
-		return m_data[index];
-	}
+    T& at(size_t index)
+    {
+        if (index >= m_size)
+            throw std::out_of_range("index is out of the bounds of the array");
 
-	void swap(fixed_size_array& other) noexcept
-	{
-		std::swap(m_data, other.m_data);
-		std::swap(m_size, other.m_size);
-	}
+        return m_data[index];
+    }
 
-	///
-	/// @brief Checks whether two arrays have the same size and contain the same sequence of elements
-	///
-	/// The elements of the array must be comparable with `==`.
-	/// 
-	bool operator==(const fixed_size_array& other) const
-	{
-		if (size() != other.size())
-			return false;
-		
-		for (size_t i = 0; i < size(); i++) {
-			if (m_data[i] != other.m_data[i])
-				return false;
-		}
+    const T& at(size_t index) const
+    {
+        if (index >= m_size)
+            throw std::out_of_range("index is out of the bounds of the array");
 
-		return true;
-	}
+        return m_data[index];
+    }
+
+    T& operator[](size_t index) noexcept
+    {
+        return m_data[index];
+    }
+
+    const T& operator[](size_t index) const noexcept
+    {
+        return m_data[index];
+    }
+
+    void swap(fixed_size_array& other) noexcept
+    {
+        std::swap(m_data, other.m_data);
+        std::swap(m_size, other.m_size);
+    }
+
+    ///
+    /// @brief Checks whether two arrays have the same size and contain the same sequence of elements
+    ///
+    /// The elements of the array must be comparable with `==`.
+    ///
+    bool operator==(const fixed_size_array& other) const
+    {
+        if (size() != other.size())
+            return false;
+
+        for (size_t i = 0; i < size(); i++) {
+            if (m_data[i] != other.m_data[i])
+                return false;
+        }
+
+        return true;
+    }
 };
 
 } // namespace
